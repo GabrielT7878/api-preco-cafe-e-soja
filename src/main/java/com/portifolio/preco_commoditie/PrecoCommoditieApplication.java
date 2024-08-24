ee694a6d3d1bfb2d9ff77a3bf3b57f7b81f97f48
Gabriel.T <gabrielfernandesthiago@hotmail.com>
2024-08-24 14:45:54 -0300

    project created

---
+++ src/main/java/com/portifolio/preco_commoditie/PrecoCommoditieApplication.java
@@ -0,0 +1,13 @@
+package com.portifolio.preco_commoditie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecoCommoditieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrecoCommoditieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portifolio/preco_commoditie/PrecoCommoditieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portifolio.preco_commoditie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecoCommoditieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
