70e82bc474dc4ae03925da8e246b75e325a69bbb
Gabriel.T <gabrielfernandesthiago@hotmail.com>
2024-09-01 18:06:44 -0300

    added tests

--- src/test/java/com/portifolio/preco_commoditie/PrecoCommoditieApplicationTests.java
+++ src/test/java/com/portifolio/preco_commoditie/PrecoCommoditieApplicationTests.java
@@ -1,13 +1,46 @@
 package com.portifolio.preco_commoditie;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient(timeout = "36000")
 class PrecoCommoditieApplicationTests {
 
+    @Autowired
+    private WebTestClient webTestClient;
+
     @Test
-    void contextLoads() {
+    public void obterPrecoSacaSucesso() {
+        webTestClient
+                .get()
+                .uri("/preco?commoditie=cafe")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("commoditie").isEqualTo("cafe");
     }
+
+    @Test
+    public void obterPrecoSacaFalhaSemPassarParametro() {
+        webTestClient
+                .get()
+                .uri("/preco")
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    public void obterPrecoSacaFalhaPassarParametroIncorreto() {
+        webTestClient
+                .get()
+                .uri("/preco?commoditie=testunitario")
+                .exchange()
+                .expectStatus().is5xxServerError();
+    }
+
 
 }
